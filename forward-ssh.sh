ef812c18779ebbc38683338c38a9fca2c9b93063
Kris Lamoureux <kris@lamoureux.io>
2023-11-03 00:12:12 -0400

    Add copyright notice on forward-ssh.sh

--- forward-ssh.sh
+++ forward-ssh.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 # Finds the SSH private key under ./.vagrant and connects to
-# the Vagrant box, port forwarding localhost ports: 8443, 80, 443
+# the Vagrant box, port forwarding localhost ports: 8443, 443, 80, 22
+#
+# Download the latest script:
+# https://git.krislamo.org/kris/homelab/raw/branch/main/forward-ssh.sh
+#
+# Copyright (C) 2023  Kris Lamoureux
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Root check
 if [ "$EUID" -ne 0 ]; then
